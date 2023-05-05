41b4b64b25ef4358a009da0bb47a555f7b2c8cb5
Tristan Wauthier <tristan.wauthier@hotmail.com>
2023-05-05 13:31:37 +0200

    [ADD] Added a Base Recorder class

--- Scripts/Simulator/Enums.cs
+++ Scripts/Simulator/Enums.cs
@@ -151,5 +151,13 @@ namespace Rhinox.XR.UnityXR.Simulator
                     return;
             }
         }
+
+
+        public enum SimulatorInputType
+        {
+            Button = 0x1,
+            Axis2D = 0x2,
+            Axis1D = 0x4
+        }
     }
 }
